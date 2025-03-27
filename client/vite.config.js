11910a35baf968a4889d5fa84d24d503e1a76bd4
unknown <bernicojc113@gmail.com>
2025-03-27 13:32:01 -0700

    nothing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  },
   server: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
     proxy: {
+      '/api': 'http://127.0.0.1:5000'
     }
   }
-})
+});
